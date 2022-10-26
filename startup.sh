007a82db739dda948747c99598095f4f9fb4aa4d
Xariwey <samuelovar@gmail.com>
2022-10-26 03:53:13 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Version 1.5.2
+# Version 1.5.3
 # This is a startup script for UniFi Controller on Debian based Google Compute Engine instances.
 # For instructions and how-to:  https://metis.fi/en/2018/02/unifi-on-gcp/
 # For comments and code walkthrough:  https://metis.fi/en/2018/02/gcp-unifi-code/
