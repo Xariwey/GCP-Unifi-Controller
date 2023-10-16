5f4134931e6f86da90128bb31d2fe8bef55cab44
Xariwey <93450968+Xariwey@users.noreply.github.com>
2023-10-16 14:54:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -167,14 +167,13 @@ function install() {
 	curl -sSO https://cloud.google.com/static/stackdriver/docs/set-permissions.sh
 	(bash set-permissions.sh --project=$project)
 
-# Better to do this inside VM
-# 	echo
-# 	echo "Enabling Ops Agent"
-# 	cat > agents_to_install.csv <<_EOF
-# "projects/${project}/zones/$zone/instances/$name-vm","[{""type"":""ops-agent""}]"
-# _EOF
-# 	curl -sSO https://dl.google.com/cloudagents/mass-provision-google-cloud-ops-agents.py
-# 	python3 mass-provision-google-cloud-ops-agents.py --file agents_to_install.csv
+	echo
+	echo "Enabling Ops Agent"
+	cat > agents_to_install.csv <<_EOF
+"projects/${project}/zones/$zone/instances/$name-vm","[{""type"":""ops-agent""}]"
+_EOF
+	curl -sSO https://dl.google.com/cloudagents/mass-provision-google-cloud-ops-agents.py
+	python3 mass-provision-google-cloud-ops-agents.py --file agents_to_install.csv
 
 	echo
 	echo "Creating Auto Patch Deployment for the $name VM"
